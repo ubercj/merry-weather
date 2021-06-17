379bb16102c365e0a2935b3ecb8c40f0cf7e09a6
ubercj <ubercj@gmail.com>
2021-06-17 19:11:59 -0400

    Added sass-loader, css-loader, and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  module: {
+    rules: [
+        {
+          test: /\.(css|scss)$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ],
+  },
   devServer: {
     contentBase: './dist'
   },
