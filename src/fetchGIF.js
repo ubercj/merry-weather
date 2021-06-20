f4067849ce1bba3614f203b33e1aeafb11b2e5b6
ubercj <ubercj@gmail.com>
2021-06-20 17:02:49 -0400

    Removed GIPHY api function

--- src/fetchGIF.js
+++ src/fetchGIF.js
@@ -19,18 +19,4 @@ const getWeatherGIF = (term) => {
   }
 }
 
-const fetchGIF = async (term) => {
-  try {
-    const response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=QRj98GT4aMFT6yvQvGtLjwOGKsJcLygh&q=${term}`,
-      {mode: 'cors'}
-    );
-    const gifData = await response.json();
-    return gifData.data[0].images.original.url;
-  } catch (e) {
-    console.log('API call failed for some reason.');
-    return 'https://i.ytimg.com/vi/r4sdwdtCiXQ/maxresdefault.jpg';
-  }
-}
-
-export { fetchGIF, getWeatherGIF };
+export { getWeatherGIF };
