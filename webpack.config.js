cae2ac80a8b9e40f6e583e3e8d1d1090a2dd0af7
ubercj <ubercj@gmail.com>
2021-07-01 17:13:18 -0400

    Added babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,19 @@ module.exports = {
         {
           test: /\.(css|scss)$/,
           use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
       }
     ],
   },
